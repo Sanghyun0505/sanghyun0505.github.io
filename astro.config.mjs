7e57e950e4edb92b300401a34d12d0831b044420
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-07-22 15:26:43 +0900

    refactor: vanilla extract -> tailwindcss로 재전환

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321/",
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [vanillaExtractPlugin()],
-  },
+  integrations: [mdx(), sitemap(), tailwind()],
 });
