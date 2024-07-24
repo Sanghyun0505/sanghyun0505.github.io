aa11b07f0d14f838c39af06ce8499d8a3ceeb75d
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-07-24 14:39:59 +0900

    feat: astro.config 옵션 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,39 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      drafts: true,
+      shikiConfig: {
+        theme: "one-dark-pro",
+        wrap: true,
+      },
+    }),
+    sitemap(),
+    tailwind(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+        },
+      ],
+    }),
+  ],
 });
